32584a182c6e5e989cf16cbcd27a6a95f8dd0b8e
ChildEats <160313048+ChildEats@users.noreply.github.com>
2024-02-18 16:38:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ e=c*b;
 f=d/e;
 g=f*e;
 h=d-g;
-console.log(f + )*/
+alert(f + )*/
 var b=2
 var c=1
 var randomNumber = Math.floor(Math.random()*10)+1;
@@ -15,8 +15,8 @@ var my_number;
 for(var i=1; i<=3;){
     my_number = prompt("Place a number between 1 to 10")
         if(my_number==randomNumber){
-            console.log("You win");
-            console.log("How many tries taken " + c)
+            alert("You win");
+            alert("How many tries taken " + c)
             break;
         }else if(my_number=="" || my_number>=10){
             alert("Wrong intreger or wrong input")
@@ -30,11 +30,11 @@ for(var i=1; i<=3;){
             alert("Number too low, Guess higher")
         }else{
             alert("Number too high, Guess lower")
-            console.log(randomNumber)
+            alert(randomNumber)
         }
     if(i==3){
-        console.log("Game over");
-        console.log("The winning number: " + randomNumber);
+        alert("Game over");
+        alert("The winning number: " + randomNumber);
     }
 }
 
